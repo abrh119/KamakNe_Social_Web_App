6758bc87377d3ea8dbad6a009adb93323564250b
Abdur Rahman <59731843+abrh119@users.noreply.github.com>
2022-06-06 18:47:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       "{url}",
     CLOUDINARY_CLOUDNAME: "{cloudname}",
     MONGO_URI:
-      "mongodb+srv://{username}:{password}@cluster0.z9i4w.mongodb.net/{docname}?retryWrites=true&w=majority",
+      "{URL}",
     PORT: 3004,
   }, //url for image upload to cloudinary. First copy "API base url" from Cloudinary. Then paste it here.
 };
