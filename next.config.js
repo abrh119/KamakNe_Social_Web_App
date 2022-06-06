c5a22eec7f0abde90f5f82575546adc937d65f8c
Abdur Rahman <59731843+abrh119@users.noreply.github.com>
2022-06-06 18:29:18 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       "https://api.cloudinary.com/v1_1/individualbookstore/image/upload",
     CLOUDINARY_CLOUDNAME: "individualbookstore",
     MONGO_URI:
-      "mongodb+srv://salman:salman123@cluster0.z9i4w.mongodb.net/kamakne?retryWrites=true&w=majority",
+      "mongodb+srv://{username}:{password}@cluster0.z9i4w.mongodb.net/{docname}?retryWrites=true&w=majority",
     PORT: 3004,
   }, //url for image upload to cloudinary. First copy "API base url" from Cloudinary. Then paste it here.
 };
